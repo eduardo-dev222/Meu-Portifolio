76014655e6295d725bb85a50ded6e1f9fe9a5e26
Eduardo Nascimento <meucomputer476@gmail.com>
2025-02-09 10:46:41 -0300

    meu portifolio simples

---
+++ app.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    const headerTitle = document.querySelector('h1');
+    headerTitle.style.transition = 'transform 0.5s ease';
+    headerTitle.style.transform = 'scale(1.2)';
+};
